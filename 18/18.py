db5a2fe6f3750e7e4170ce5c8f8ac90749d02596
Marius Kristensen <marius.kristensen@gmail.com>
2015-12-18 08:15:23 +0100

    Luke 18

---
+++ 18/18.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+num = [2907, 6165, 6129, 3468, 2040, 4331, 7935, 5683, 6004, 9694, 8092, 188, 5796, 1184, 8873, 3200, 1981, 9556, 9981,
+       1387, 7802, 8387, 9970, 7326, 5372, 28, 628, 3408, 6, 3425, 3071, 6021, 9989, 5077, 824, 938, 1399, 5607, 6973,
+       5703, 9609, 4398, 8247, 5164, 2026, 4, 4468, 9524, 8, 9227, 8969, 1746, 5593]
+
+
+def do_append(args):
+    appended_num = str(args)
+    while len(appended_num) < 4:
+        appended_num += appended_num[0]
+    return int(appended_num)
+
+print ''.join(str(i) for i in sorted(num, key=do_append, reverse=True))
